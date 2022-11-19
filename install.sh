c89a77e9f7ab2bf34992472c151a6d81f6bfffb6
shenyu <shenyu@shenyu.me>
2022-11-19 13:20:24 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -31,7 +31,8 @@ npm install neovim -g
 gem install neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-ln -s $HOME/.dosfiles/.config/nvim  $HOME/.config/
+mkdir $HOME/.config
+ln -s $HOME/.dotfiles/.config/nvim  $HOME/.config/
 nvim -c 'PlugInstall --sync|qa'
 nvim -c 'CocUpdateSync|q'
 nvim -c 'TSInstallSync all|q'
